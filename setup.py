daefa9b96edd3f5b4273c4755bcd3d4c5af11b1c
Pat Migliaccio <pat@patmigliaccio.com>
2019-07-17 13:58:36 -0400

    Add setup.py file for installation via pip

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+   name='skipthoughts',
+   version='0.0.1',
+   description='Sent2Vec encoder and training code from the paper Skip-Thought Vectors.',
+   packages=['skipthoughts']
+)
